2cc4ae5b199bb9cc8f44cca0b3da4cab84d8dc2e
achrafezzaam <achraf.ezzaam@gmail.com>
2024-04-18 15:34:22 +0100

    Rolling back on the 8-make_multiplier.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,4 +4,4 @@
 
 def concat(str1: str, str2: str) -> str:
     ''' Concatanate two given strings '''
-    return str(a + b)
+    return f"{str1}{str2}"
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,4 +5,4 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     ''' Returns a function that multiplies a float by multiplier '''
-    return float(lambda x: x * multiplier)
+    return lambda x: x * multiplier
