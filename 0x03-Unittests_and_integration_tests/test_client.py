bd33443dddfc9b404079d321ba49968b0d7ec316
achrafezzaam <achraf.ezzaam@gmail.com>
2024-05-13 23:35:16 +0100

    Adding the test methods for the org and _public_repos_url methods

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,43 @@
+''' Test the client file '''
+from client import GithubOrgClient
+import unittest
+from unittest.mock import MagicMock, Mock, patch, PropertyMock
+from typing import Dict
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    ''' Test the GithubOrgClient class '''
+    @parameterized.expand([
+        ("google", {"login": "google"}),
+        ("abc", {"login": "abc"})
+    ])
+    @patch("client.get_json")
+    def test_org(
+            self,
+            org: str,
+            res: Dict,
+            fn: MagicMock
+            ) -> None:
+        ''' Test the org method '''
+        fn.return_value = MagicMock(return_value=res)
+        client = GithubOrgClient(org)
+        self.assertEqual(client.org(), res)
+        fn.asser_called_once_with(
+                "https://api.github.com/orgs/{org}"
+                .format(org=org)
+                )
+
+    def test_public_repos_url(self) -> None:
+        """Tests the _public_repos_ur method"""
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable = PropertyMock,
+                ) as org_mock:
+            org_mock.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
